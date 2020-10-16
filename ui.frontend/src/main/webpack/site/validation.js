3a69c660ccdf0e8ed4ba30034d0f9021b13c38d7
vibhavcg <72173702+vibhavcg@users.noreply.github.com>
2020-10-16 12:20:01 +1100

    Validation optimized

--- ui.frontend/src/main/webpack/site/validation.js
+++ ui.frontend/src/main/webpack/site/validation.js
@@ -1,20 +1,40 @@
-const email = document.querySelector('input[type="email"]');
-email.addEventListener('focus', (event) => {
-  event.target.classList.remove('error-message');
-  var element = document.getElementById('constraintmsg');
+function focusElement(event) {
+  event.classList.remove('error-message');
+  var element = event.parentElement.querySelector('#constraintmsg');
   if(element){
-    element.parentElement.removeChild(element);
+    event.parentElement.removeChild(element);
   }
-});
+}
+function validateElement(event) {
+  var eventparent = event.parentElement;
+  if (event.validity){
+    var errormessage = document.createElement("div");
+    errormessage.classList.add('constraint-message');
+    errormessage.setAttribute("id","constraintmsg");
+    event.classList.add('error-message');
+    if (event.validity.valueMissing){
+      errormessage.appendChild(document.createTextNode(eventparent.getAttribute('data-cmp-required-message')));
+    }
+    if (event.validity.typeMismatch){
+      errormessage.appendChild(document.createTextNode(eventparent.getAttribute('data-cmp-constraint-message')));
+   }
+     eventparent.appendChild(errormessage);
+ }
+}
 
-email.addEventListener('blur', (event) => {
-  if (event.target.validity.typeMismatch){
-    event.target.classList.add('error-message');
-      var errormessage = document.createElement("div");
-      errormessage.classList.add('constraint-message');
-      errormessage.setAttribute("id","constraintmsg");
-      var text = document.createTextNode(event.target.parentElement.getAttribute('data-cmp-constraint-message'));
-      errormessage.appendChild(text);
-    event.target.parentElement.appendChild(errormessage);
+const text = document.querySelectorAll('input[type="text"], input[type="email"],input[type="tel"]');
+for (i = 0; i < text.length; i++)
+  {
+    text[i].addEventListener('focus', (event) => { focusElement(event.target);});
+    text[i].addEventListener('blur', (event) => { validateElement(event.target);});
   }
-});
+
+const submit = document.querySelector('button[type="SUBMIT"]');
+  submit.addEventListener('click', (event) => {
+    const inputField = event.target.parentElement.parentElement.getElementsByTagName('input');
+    for (i = 0; i < inputField.length; i++)
+    {
+      focusElement(inputField[i]);
+      validateElement(inputField[i]);
+    }
+  });
